0218eb65e32f5a3fbd315ed62db3127b375771c9
Dmitriy Merkurev <dmerkurev@avito.ru>
2017-11-17 03:37:54 +0300

    fix client interface

--- client.go
+++ client.go
@@ -6,6 +6,7 @@ type Client interface {
 	Devices() ([]model.DeviceIdentifier, error)
 	DeviceInfo(device model.DeviceIdentifier) (*model.Device, error)
 	Push(device model.DeviceIdentifier, from, to string) error
-	Install(device model.DeviceIdentifier, from, to string) error
+	Pull(device model.DeviceIdentifier, from, to string) error
+	Install(device model.DeviceIdentifier, from string) error
 	RunInstrumentationTests(device model.DeviceIdentifier, params model.InstrumentationParams) (*model.InstrumentationResult, error)
 }
